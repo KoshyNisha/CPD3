4a2233cbcffa63d8ca66d446d5de83f70ddd12cb
Nisha Koshy <koshyn@umich.edu>
2024-10-27 20:34:22 -0400

    adding py and html files

--- csv_to_meets_html.py
+++ csv_to_meets_html.py
@@ -29,8 +29,8 @@ def csv_to_html(csv_filename, output_folder):
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>{link_text}</title>
-<link rel="stylesheet" href="css/reset.css">
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="reset.css">
+<link rel="stylesheet" href="style.css">
 </head>
    <body>
    <a href = "#main">Skip to Main Content</a>
